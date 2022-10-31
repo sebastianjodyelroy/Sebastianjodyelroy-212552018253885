b8708b11449193ea6c771e0363b5e04d2a99b5b5
sebastianjodyelroy <82053553+sebastianjodyelroy@users.noreply.github.com>
2022-10-31 10:55:41 +0700

    Add files via upload

---
+++ pertemuan3dan4.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+    <?php 
+// MATERI 3
+      $nama = "Sebastian Jody Elroy"; /* ini adalah variabel          nama, ber value string */
+      $umur = 20; // ini adalah variabel umur, ber value int
+      $nilai = 90.90; // ini adalah variabel umur, ber value float/koma
+      echo "<h2>Welcome</h2>"; 
+      echo "<p>Disini adalah isi paragraf</p>"; 
+      echo "halo namaku adalah ".$nama." umurku sudah ".$umur." tahun";
+      echo "<br>"; 
+      echo "<br>";
+
+
+
+// MATERI 4
+      $a = 7;
+      $b = 5;
+      $c = 2;
+      echo "hasil dari perkalian ".$a." dan ".$b." adalah ".$a*$b."<br>";
+    ?> 
+   text ini diluar dari pada php
+  </body>
+</html>
