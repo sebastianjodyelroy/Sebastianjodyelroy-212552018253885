168736a7659e6183e2e804b8125e95dafaced47a
sebastianjodyelroy <82053553+sebastianjodyelroy@users.noreply.github.com>
2022-10-31 10:04:43 +0700

    Add files via upload

---
+++ php5-1.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>Belajar PHP</title>
+  </head>
+  <body>
+
+    <h1>Array PHP</h1>
+    
+    <?php
+
+    $hewan = array('ayam','buaya','kuda');
+    $nama = ['Sebastian', 'Jody', 'Elroy'];
+
+    print_r($hewan);
+    echo $nama[0];
+
+    ?>
+
+  </body>
+</html>
